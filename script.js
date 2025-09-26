7a0c417f88faf81b6ffb4eb36de622d32a594135
jkrawlin <87916316+jkrawlin@users.noreply.github.com>
2025-09-26 04:39:02 +0300

    feat(ui-excel): overhaul employee layout to Excel-like ribbon/table/status bar; add row numbers; gridline styling

--- script.js
+++ script.js
@@ -326,8 +326,9 @@ function renderEmployeeTable() {
     }
   }
 
-  tbody.innerHTML = sorted.map((employee) => `
+  tbody.innerHTML = sorted.map((employee, idx) => `
     <tr>
+      <td class="row-header">${idx + 1}</td>
       <td>${employee.name}</td>
       <td>${employee.email}</td>
       <td>${employee.position}</td>
